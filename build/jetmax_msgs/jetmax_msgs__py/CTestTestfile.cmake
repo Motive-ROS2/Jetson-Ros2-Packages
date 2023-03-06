bbff1273e2664c4959e3bfe14f9297ad8542f313
minhhuynh <huynhmin@oregonstate.edu>
2023-03-06 01:12:22 -0800

    Revert "push packages from robot"
    This reverts commit 47eeba8c09945a1d36180e6e678245e7921a9e0c.

--- build/jetmax_msgs/jetmax_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hiwonder/capstone_ws/src/Jetson-Ros2-Packages/build/jetmax_msgs/jetmax_msgs__py
-# Build directory: /home/hiwonder/capstone_ws/src/Jetson-Ros2-Packages/build/jetmax_msgs/jetmax_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
